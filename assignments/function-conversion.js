3836b83210acf81091661d624786fdef1523d3d8
Mariam Adedeji <maadedji76@student.lautech.edu.ng>
2019-08-19 13:18:38 +0100

    solutions done to questions in function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,31 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+myFunction = () => {
+    console.log("Function was invoked!");
+}
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+anotherFunction = (param) => param;
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
 // Stretch
 
@@ -28,3 +37,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+triple = exampleArray.map ((num) => num * 3);
+console.log(triple);
